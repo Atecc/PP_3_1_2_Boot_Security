af3f9f60cb1c8d2877d7029746ddc5cc1e50a9fd
Atecc <rizvan1127@gmail.com>
2023-12-06 14:29:31 +0300

    HasAnyRole

--- src/main/java/ru/kata/spring/boot_security/demo/controller/RESTUserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/RESTUserController.java
@@ -14,7 +14,7 @@ import ru.kata.spring.boot_security.demo.service.UserService;
 import java.security.Principal;
 @RestController
 @RequestMapping("/api/user")
-@PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+@PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
 public class RESTUserController {
     private final UserService userService;
 
